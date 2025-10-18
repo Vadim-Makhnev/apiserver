dd078e8451b7f54a37c58bc1ac83124731c27b6b
Vadim <vadik.mahnev@yandex.ru>
2025-10-18 17:00:23 +0300

    add:Store field for config file

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,9 +1,12 @@
 package apiserver
 
+import "github.com/Vadim-Makhnev/apiserver/internal/app/store"
+
 // Config represents the structure of the config
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level"`
+	Store    *store.Config
 }
 
 // NewConfig creates a new Config
@@ -11,5 +14,6 @@ func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
+		Store:    store.NewConfig(),
 	}
 }
