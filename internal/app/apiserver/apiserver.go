5a17b43f112eddcd1104bf637a99e8eebdc9ec28
Vadim <vadik.mahnev@yandex.ru>
2025-10-18 16:59:37 +0300

    add:configure store method

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -34,6 +34,10 @@ func (s *APIServer) Start() error {
 
 	s.configureRouter()
 
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	s.logger.Info("Starting api server")
 
 	return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -58,4 +62,14 @@ func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello")
 	}
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+
+	return nil
 }
